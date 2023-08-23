eee3089d79c17c397a0fcdae6118e1f5f16b6b82
jialin <jialinkuang@126.com>
2023-08-23 11:54:51 +0800

    chore: test var

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "ports" {
   description = "Service Ports"
   value       = var.ports
 }
+
+# @label "Test Environment Variables"
+# @group "test"
+variable "test_env" {
+  type        = map(string)
+  description = "Name and value pairs to set as the environment variables"
+  default     = {}
+}
+# @label "Test Replicas"
+# @group "test"
+variable "test_replicas" {
+  type        = number
+  description = "Replicas to deploy"
+  default     = 1
+}
